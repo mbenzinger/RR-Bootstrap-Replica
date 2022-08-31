98772bef598f36e6bcbe2a7218adc77664f8e979
Mason Benzinger <mason.benzinger@gmail.com>
2022-08-31 15:13:09 -0500

    added in code from excersize, some solution code, and soem fixes when things didn't work

---
+++ src/components/TopBar.js
@@ -0,0 +1,23 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
+export default function TopBar() {
+    return (
+<Navbar bg="light" expand="lg" >
+    <Navbar.Brand style={{margin: "5px 0 0 20px"}} href="#home">
+        <img src='.\public\instacart-logo-color.svg' /> 
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto"> 
+        </Nav>
+        <Nav>
+            <Nav.Link href="#LogIn">Log In</Nav.Link>
+            <Button variant="success">Sign Up</Button>{' '}
+        </Nav>
+    </Navbar.Collapse>
+</Navbar>
+    )
+}
+
